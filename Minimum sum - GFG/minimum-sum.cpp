609fd2475a4d4cb7c23dd0969a929a8cf6a8198d
gautam pandey <90997083+gkpande@users.noreply.github.com>
2023-03-08 11:19:59 +0530

    Added solution - LeetHub

--- Minimum sum - GFG/minimum-sum.cpp
+++ Minimum sum - GFG/minimum-sum.cpp
@@ -13,11 +13,14 @@ public:
       sort(arr, arr + n);
         string a="";
         string b="";
-        for (int i = 0; i < n; i += 2) {
+        for (int i = 0; i < n; i ++) {
+            if(i%2==0){
              a += (arr[i] + '0');
         }
-        for (int i = 1; i < n; i += 2) {
+        else
+        {
              b += (arr[i] + '0');
+        }
         }
         
         int j = a.length() - 1;
@@ -60,68 +63,7 @@ public:
             }
         reverse(ans.begin(),ans.end());
         return ans;
-    // sort(arr, arr + n);
- 
-    // Two String for storing our two minimum numbers
-    // string a = "", b = "";
- 
-    // for (int i = 0; i < n; i += 2) {
-    //     a += (arr[i] + '0');
-    // }
-    // for (int i = 1; i < n; i += 2) {
-    //     b += (arr[i] + '0');
-    // }
- 
-    // int j = a.length() - 1;
-    // int k = b.length() - 1;
- 
-    // // as initial carry is zero
-    // int carry = 0;
-    // string ans = "";
-    // while (j >= 0 && k >= 0) {
-    //     int sum = 0;
-    //     sum += (a[j] - '0') + (b[k] - '0') + carry;
-    //     ans += to_string(sum % 10);
-    //     carry = sum / 10;
-    //     j--;
-    //     k--;
-    // }
- 
-    // If string b is over and string a is left
-    // here we don't need to put here while condition
-    // as it would run at max one time. Because the
-    // difference between both the strings could be at
-    // max 1.
-    // while (j >= 0) {
-    //     int sum = 0;
-    //     sum += (a[j] - '0') + carry;
-    //     ans += to_string(sum % 10);
-    //     carry = sum / 10;
-    //     j--;
-    // }
- 
-    // If string a is over and string b is left
-    // while (k >= 0) {
-    //     int sum = 0;
-    //     sum += (b[k] - '0') + carry;
-    //     ans += to_string(sum % 10);
-    //     carry = sum / 10;
-    //     k--;
-    // }
-    // // if carry is left
-    // if (carry) {
-    //     ans += to_string(carry);
-    // }
- 
-    // // to remove leading zeroes as they will be ahead of our
-    // // sum
-    // while (!ans.empty() and ans.back() == '0')
-    //     ans.pop_back();
- 
-    // // reverse our final string because we were storing sum
-    // // from left to right
-    // reverse(ans.begin(), ans.end());
-    // return ans;
+    
     }
 };
 
