c70a7e594d38bf292556f04484a6fc22f463ec97
gautam pandey <90997083+gkpande@users.noreply.github.com>
2023-03-13 11:34:28 +0530

    Added solution - LeetHub

---
+++ Min Cost Climbing Stairs - GFG/min-cost-climbing-stairs.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+  //recusrsive method
+  int solve(vector<int>&cost ,int n){
+      if(n==0){
+          return cost[0];
+      }
+      if(n==1){
+          return cost[1];
+      }
+      int ans=cost[n]+min(solve(cost,n-1),solve(cost,n-2));
+      return ans;
+  }
+  //recursive + memorization
+  int solve(vector<int>&cost ,int n,vector<int>&dp){
+      if(n==0){
+          return cost[0];
+      }
+      if(n==1){
+          return cost[1];
+      }
+      if(dp[n]!=-1){
+          return dp[n];
+      }
+      
+      dp[n]=cost[n]+min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+      return dp[n];
+  }
+    int minCostClimbingStairs(vector<int>&cost ,int N) {
+        // to go at top N we require min step if came from N-1 or N-2.
+        // int ans=min(solve(cost,N-1),solve(cost,N-2));
+        // return ans;
+        vector<int>dp(N+1,-1);
+        int ans=min(solve(cost,N-1,dp),solve(cost,N-2,dp));
+        return ans;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        vector<int>cost(N);
+        
+        for(int i=0 ; i<N ; i++){
+            cin>>cost[i];
+        }
+
+        Solution ob;
+        cout<<ob.minCostClimbingStairs(cost,N);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
