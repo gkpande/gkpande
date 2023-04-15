ced98a031d28469558187ab70c0e54c3ab490057
gautam pandey <90997083+gkpande@users.noreply.github.com>
2023-04-15 22:16:17 +0530

    Added solution - LeetHub

---
+++ Median of BST - GFG/median-of-bst.cpp
@@ -0,0 +1,193 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+float findMedian(struct Node* node);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+ 
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+int main() {
+  
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   //cout << t << endl;
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root = buildTree(s);
+
+      // getline(cin, s);
+       
+        cout << findMedian(root) << endl;
+
+      // cout<<"~"<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
+
+
+/*
+Structure of the binary Search Tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+Node *findPred(Node* root){
+    Node* temp=root->left;
+    while(temp->right!=NULL && temp->right!=root){
+        temp=temp->right;
+    }
+    return temp;
+}
+float findMedian(struct Node *root)
+{
+      //find total number of nodes using moris inorder traversal
+      struct Node* curr=root;
+      int count=0;
+      while(curr!=NULL){
+          if(curr->left==NULL){
+              count++;
+              curr=curr->right;
+          }
+          else{
+              struct Node* pred=findPred(curr);
+              if(pred->right==NULL){
+                  pred->right=curr;
+                  curr=curr->left;
+              }
+              else{
+                  pred->right=NULL;
+                  count++;
+                  curr=curr->right;
+              }
+          }
+      }
+      //find the point of median unsing moris in order traversal.
+        int currCount=0;
+      Node* prev=NULL;
+        curr=root;
+        while(curr!=NULL){
+          if(curr->left==NULL){
+              currCount++;
+              // Odd case
+            if (count % 2 != 0 && currCount == (count+1)/2)
+                return curr->data;
+ 
+            // Even case
+            else if (count % 2 == 0 && currCount == (count/2)+1)
+                return (prev->data + curr->data)/2.0;
+            prev = curr;
+            curr=curr->right;
+          }
+          else{
+              struct Node* pred=findPred(curr);
+              if(pred->right==NULL){
+                  pred->right=curr;
+                  curr=curr->left;
+              }
+              else{
+                  pred->right=NULL;
+                  currCount++;
+                    // Odd case
+                if (count % 2 != 0 && currCount == (count+1)/2)
+                    return curr->data;
+ 
+                // Even case
+                 else if (count % 2 == 0 && currCount == (count/2)+1)
+                    return (prev->data + curr->data)/2.0;
+                prev = curr;
+                curr=curr->right;
+              }
+          }
+          
+      }
+}
+
