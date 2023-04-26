1a911d26177261d1f3fdae04fe4df56d5c36fa62
gautam pandey <90997083+gkpande@users.noreply.github.com>
2023-04-26 12:48:45 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -41,14 +41,35 @@ int solveMem(int N, int W, int val[], int wt[],int index,vector<vector<int>>&dp)
     dp[index][W]= max(take,nTake);
     return dp[index][W];
 }
+int solveTab(int N, int W, int val[], int wt[]){
+    vector<vector<int>>dp(N,vector<int>(W+1,0));
+    for(int w=0;w<=W;w++){
+        dp[N-1][w]= int(w/wt[N-1])* val[N-1];
+    }
+    for(int i=N-2;i>=0;i--){
+        for(int w=0;w<=W;w++ ){
+            int take =0;
+            if(wt[i]<=w){
+                take=val[i]+dp[i][w-wt[i]];
+            }
+            int nTake=0+dp[i+1][w];
+            dp[i][w]= max(take,nTake);
+        }
+    }
+    return dp[0][W];
+    
+}
+
 
     int knapSack(int N, int W, int val[], int wt[])
     {
         //------recursive
         // return solveRec(N,W,val,wt,0);
         //-----rec+mem
-        vector<vector<int>>dp(N,vector<int>(W+1,-1));
-        return solveMem(N,W,val,wt,0,dp);
+        // vector<vector<int>>dp(N,vector<int>(W+1,-1));
+        // return solveMem(N,W,val,wt,0,dp);
+        //-----tabulation
+        return solveTab(N,W,val,wt);
     }
 };
 
