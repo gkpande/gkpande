bcb73a549090eed42d0f9e315ccd71cdcc1c334c
gautam pandey <90997083+gkpande@users.noreply.github.com>
2023-05-06 20:04:36 +0530

    Added solution - LeetHub

--- Count all possible paths from top left to bottom right - GFG/count-all-possible-paths-from-top-left-to-bottom-right.cpp
+++ Count all possible paths from top left to bottom right - GFG/count-all-possible-paths-from-top-left-to-bottom-right.cpp
@@ -39,12 +39,25 @@ class Solution {
       dp[i][j]=(d+r)%1000000007;
       return dp[i][j];
   }
+  int solveTab(int i,int j,int m,int n){
+      vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+      for(int i=0;i<n;i++){
+          dp[m-1][i]=1;
+      }
+      for(int i=m-2;i>=0;i--){
+          for(int j=n-1;j>=0;j--){
+              dp[i][j]=(dp[i+1][j]+dp[i][j+1])%1000000007;
+          }
+      }
+      return dp[0][0];
+  }
     long long int numberOfPaths(int m, int n){
        //recursive
     //   return solveRec(0,0,m,n);
     //rec+mem
-    vector<vector<int>>dp(m,vector<int>(n,-1));
-    return solveMem(0,0,m,n,dp);
+    // vector<vector<int>>dp(m,vector<int>(n,-1));
+    // return solveMem(0,0,m,n,dp);
+    return solveTab(0,0,m,n);
     }
 };
 
