f5530ebb5169d3cdddbd8a91e60ae70899de8128
gautam pandey <90997083+gkpande@users.noreply.github.com>
2023-05-15 23:37:20 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -51,19 +51,51 @@ int solveMem(int i,int j,vector<vector<int>>&Matrix,int N,vector<vector<int>>&dp
     dp[i][j]=max(ans1,max(ans2,ans3));
     return dp[i][j];
 }
+int solveTab(int N,vector<vector<int>> Matrix){
+    vector<vector<int>>arr(N,vector<int>(N,0));
+    for(int i=0;i<N;i++){
+        arr[N-1][i]=Matrix[N-1][i];
+    }
+    for(int i=N-2;i>=0;i--){
+        for(int j=N-1;j>=0;j--){
+            int ans1=0,ans2=0,ans3=0;  
+            //lower digonal
+            if(i+1<N && j-1>=0){
+                ans1=Matrix[i][j]+arr[i+1][j-1];
+            }
+            //lower
+            if(i+1<N){
+                ans2=Matrix[i][j]+arr[i+1][j];
+            }
+            //lower right diagnola
+            if(i+1<N && j+1<N){
+                ans3=Matrix[i][j]+arr[i+1][j+1];
+            }
+            arr[i][j]=max(ans1,max(ans2,ans3));
+        }
+    }
+    int maxPath=INT_MIN;
+    for(int i=0;i<N;i++){
+            maxPath=max(maxPath,arr[0][i]);
+    }
+    return maxPath;
+}
     int maximumPath(int N, vector<vector<int>> Matrix)
     {
-        int maxPath=INT_MIN;
+        // int maxPath=INT_MIN;
         // for(int i=0;i<N;i++){
         //     int ans=solveRec(0,i,Matrix,N);
         //     maxPath=max(maxPath,ans);
         // }
-        vector<vector<int>>dp(N,vector<int>(N,-1));
-        for(int i=0;i<N;i++){
-            int ans=solveMem(0,i,Matrix,N,dp);
-            maxPath=max(maxPath,ans);
-        }
-        return maxPath;
+        //memorization
+        // vector<vector<int>>dp(N,vector<int>(N,-1));
+        // for(int i=0;i<N;i++){
+        //     int ans=solveMem(0,i,Matrix,N,dp);
+        //     maxPath=max(maxPath,ans);
+        // }
+        // return maxPath;
+        //tabulation
+        return solveTab(N,Matrix);
     }
 };
 
