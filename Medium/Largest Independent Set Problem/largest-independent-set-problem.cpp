c3999a007c355ea0a162de93ee47b270911ac856
gautam pandey <90997083+gkpande@users.noreply.github.com>
2023-07-21 11:02:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Largest Independent Set Problem/largest-independent-set-problem.cpp
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+// you are required to complete this function
+// function should return the size of the 
+// Largest Independent set in the tree
+
+
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+int solveRec(Node* root){
+    if(root==NULL){
+        return 0;  //LISS is zero
+    }
+    //not include root in lis
+    int incl=solveRec(root->left)+solveRec(root->right);
+    //include root
+    int notIncl=1;
+    if(root->left){
+        notIncl +=solveRec(root->left->left)+solveRec(root->left->right);
+    }
+    if(root->right){
+        notIncl +=solveRec(root->right->left)+solveRec(root->right->right);
+    }
+    return max(incl,notIncl);
+}
+int LISS(Node *root)
+{
+    //a subset is LISS if no node in subset is adjecent
+    return solveRec(root);
+}
+
+
+//{ Driver Code Starts.
+int main()
+{
+  int t;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+    string treeString;
+	getline(cin,treeString);
+	Node* root = buildTree(treeString);
+    cout<<LISS(root)<<endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
