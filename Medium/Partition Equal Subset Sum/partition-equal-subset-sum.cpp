204fb36f6ae6ed262c9a14f9d828810c9e516db1
gautam pandey <90997083+gkpande@users.noreply.github.com>
2023-09-15 16:58:15 +0530

    Updated Solution - GfG to GitHub

--- Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
+++ Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -21,6 +21,59 @@ public:
         }
         return solveRec(n-1,arr,sum-arr[n-1])||solveRec(n-1,arr,sum);
     }
+    int solveMem(int n,int arr[],int sum,vector<vector<int>>&dp){
+        if(sum==0){
+            return 1;
+        }
+        if(sum!=0 && n==0){
+            return 0;
+        }
+        if(dp[sum][n]!=-1){
+            return dp[sum][n];
+        }
+        if(arr[n-1]>sum){
+            return solveRec(n-1,arr,sum);
+        }
+        dp[sum][n]= solveMem(n-1,arr,sum-arr[n-1],dp)||solveMem(n-1,arr,sum,dp);
+        return dp[sum][n];
+    }
+    int solveTab(int n,int arr[],int sum){
+        vector<vector<int>>dp(sum,vector<int>(n+1,0));
+        for(int i=0;i<=n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<=sum;i++){
+            for(int j=1;j<=n;j++){
+                if(arr[j-1]>i){
+                    dp[i][j]=dp[i][j-1];
+                }else{
+                    dp[i][j]= dp[i-arr[j-1]][j-1]||dp[i][j-1];
+                }
+                
+            }
+        }
+        
+        
+        return dp[sum][n];
+    }
+    int spaceOp(int n,int arr[],int sum){
+        vector<int>next(sum+1,0);
+        next[0]=1;
+        
+        vector<int>curr(sum+1,0);
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=sum;j++){
+                int incl=0;
+                if(j-arr[i]>=0){
+                    incl=next[j-arr[i]];
+                }
+                int excl=next[j];
+                curr[j]=incl||excl;
+            }
+            next=curr;
+        }
+        return next[sum];
+    }
     int equalPartition(int N, int arr[])
     {
         int sum=0;
@@ -30,9 +83,14 @@ public:
         if(sum%2==1){
             return 0;
         }
-        return solveRec(N,arr,sum/2);
+        // return solveRec(N,arr,sum/2);
+        // vector<vector<int>>dp(sum/2+1,vector<int>(N+1,-1));
+        // return solveMem(N,arr,sum/2,dp);
+        // return solveTab(N,arr,sum/2);
+        return spaceOp(N,arr,sum/2);
     }
 };
+
 
 //{ Driver Code Starts.
 
